6f006803a8483bb44ce1157e37efa5d4b043acd0
Vasiliev Nikita <sharpyfox@gmail.com>
2012-12-02 19:56:00 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowerkeeper::Application.config.secret_token = '3e10787f1f8d24dde73c5b2304e48911ad767742586cdcbb6ce896751cd5e22abf04fb4d5772bd6e91be8b03205c415502e4c54e820e21dec9349345f41ed56d'
